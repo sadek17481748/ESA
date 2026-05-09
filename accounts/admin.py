692890916c010a19d5af2218f60a952d8014b48a
eventiservicesandhelp-ops <eventiservicesandhelp-ops@users.noreply.github.com>
2026-05-09 21:51:13 +0100

    feat(accounts): register User in Django admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    list_display = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "role",
+        "is_staff",
+    )
+    list_filter = ("role", "is_staff", "is_superuser", "is_active")
+    fieldsets = DjangoUserAdmin.fieldsets + (("Role", {"fields": ("role",)}),)
+    add_fieldsets = DjangoUserAdmin.add_fieldsets + (("Role", {"fields": ("role",)}),)
