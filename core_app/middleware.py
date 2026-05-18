77c126760928842285b4dad0245d622ec81c9337
sadek17481748 <sadek_j8@outlook.com>
2026-05-18 17:59:23 +0100

    feat(core_app): add TenantMiddleware for request.tenant_school

---
+++ core_app/middleware.py
@@ -0,0 +1,13 @@
+"""Attach tenant school to the request for template views and logging."""
+
+
+class TenantMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        request.tenant_school = None
+        user = getattr(request, 'user', None)
+        if user and user.is_authenticated and user.school_id:
+            request.tenant_school = user.school
+        return self.get_response(request)
