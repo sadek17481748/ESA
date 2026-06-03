5e0915d82f6f89b0b0d6257887c65d064457257f
sadek17481748 <sadek_j8@outlook.com>
2026-06-03 14:00:00 +0100

    Fix TenantMiddleware line break in settings stack

--- core/settings.py
+++ core/settings.py
@@ -64,7 +64,8 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'core_app.middleware.TenantMiddleware',    'django.contrib.messages.middleware.MessageMiddleware',
+    'core_app.middleware.TenantMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
